4dd5f6181a466b193741f4145b3e7b71a800552d
Geektutor <sodiq.akinjobi@gmail.com>
2019-06-26 20:48:33 +0100

    Edited teams

---
+++ team.js
@@ -0,0 +1,16 @@
+function m1() {
+    var x = document.getElementById("1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function m2() {
+    var x = document.getElementById("2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
