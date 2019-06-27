22cd6915ba44fed1a1d8e11a0f59500e6cd2ba52
Geektutor <sodiq.akinjobi@gmail.com>
2019-06-27 18:03:33 +0100

    Updated the php files

---
+++ php/phptemp.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Template Name: Showcase Template
+*/
+do_action( 'genesis_doctype' );
+do_action( 'genesis_title' );
+// do_action( 'genesis_meta' ); //* Uncomment this if you want child theme's style.css to load
+wp_head(); //* we need this for plugins
+?>
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://unilagconsult.com.ng/cybercamp19/wp/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cyber Camp | Showcase</title>
+</head>
+
+<body>
+
+
+
+</body>
+<script src="https://unilagconsult.com.ng/cybercamp19/wp/js/index.js"></script>
+</html>
